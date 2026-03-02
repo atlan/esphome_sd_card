c7c015f49c9aba9ed5f10cb3c7b2745267374a3f
Jürgen Schuch <juergen@atmage.de>
2026-03-02 09:20:29 +0100

    Update sd_mmc_card_esp_idf.cpp

--- components/sd_mmc_card/sd_mmc_card_esp_idf.cpp
+++ components/sd_mmc_card/sd_mmc_card_esp_idf.cpp
@@ -3,8 +3,8 @@
 #ifdef USE_ESP_IDF
 #include "math.h"
 #include "esphome/core/log.h"
-#include "vfs/include/esp_vfs.h"
-#include "fatfs/vfs/esp_vfs_fat.h"
+#include "esphome/components/vfs/include/esp_vfs.h"
+#include "esphome/components/fatfs/vfs/esp_vfs_fat.h"
 #include "sdmmc_cmd.h"
 #include "driver/sdmmc_host.h"
 #include "driver/sdmmc_types.h"
